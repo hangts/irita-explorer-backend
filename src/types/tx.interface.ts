1fdc9a92ac47a62e44b40cf577848be02ca1018b
hangTaishan <hangTaishan@bishuilantiandeMacBook-Pro.local>
2020-06-03 15:10:02 +0800

    #get /txs

--- src/types/tx.interface.ts
+++ src/types/tx.interface.ts
@@ -1,12 +1,6 @@
 import { IQueryBase } from '.';
 import { Document } from 'mongoose';
 
-export interface IDenom extends Document {
-    name:string;
-    json_schema:string;
-    creator:string;
-}
-
 export interface ITxsQueryParams extends IQueryBase {
 	type?:string,
 	status?:number,
