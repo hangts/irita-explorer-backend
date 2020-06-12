386356f445ab10c49ca132c468f062c197bbe219
lsc949982212 <949982212@qq.com>
2020-06-12 15:34:57 +0800

    resolve comments

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-/*import cluster from 'cluster';
-
-
-import os from 'os';
-const numCPUs = os.cpus().length;
-
-if (cluster.isMaster) {
-    console.log(`主进程 ${process.pid} 正在运行`);
-
-    // 衍生工作进程。
-    for (let i = 0; i < numCPUs; i++) {
-        cluster.fork();
-    }
-
-    cluster.on('exit', (worker, code, signal) => {
-        console.log(`工作进程 ${worker.process.pid} 已退出`);
-    });
-} else {
-
-    console.log(`工作进程 ${process.pid} 已启动`);
-}*/
-
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     const options = new DocumentBuilder()
