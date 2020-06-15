ea8f6a714b68a590193f88cbcfeb2ba8d3af3051
zhjibi742626083 <jinbiao@bianjie.ai>
2020-06-15 15:56:32 +0800

    delete config

--- src/config.ts
+++ src/config.ts
@@ -11,9 +11,10 @@ const {
     FAULT_TOLERANCE_EXECUTE_TIME,
     SYNC_TX_SERVICE_NAME_SIZE,
     HEARTBEAT_RATE,
+    VALIDATORS_EXECTUTE_TIME
 } = process.env;
 
-/*export const cfg = {
+export const cfg = {
     env: NODE_ENV,
     dbCfg: {
         user: DB_USER,
@@ -33,36 +34,8 @@ const {
             nft:NFT_EXECUTE_TIME || '21 * * * * *',
             txServiceName:TX_SERVICE_NAME_EXECUTE_TIME || '30 * * * * *',
             faultTolerance:FAULT_TOLERANCE_EXECUTE_TIME || '41 * * * * *',
+            validators: VALIDATORS_EXECTUTE_TIME || '1 * * * * *'
         },
         syncTxServiceNameSize: Number(SYNC_TX_SERVICE_NAME_SIZE) || 100,
-    }
-};*/
-export const cfg = {
-    env: 'development',
-    dbCfg: {
-        user: 'csrb',
-        psd: 'csrbpassword',
-        dbAddr: '192.168.150.33:37017',
-        dbName: 'sync2',
-    },
-    serverCfg:{
-        lcdAddr:'http://10.2.10.130:2317'
-    },
-    taskCfg:{
-        syncTxServiceNameSize: Number(SYNC_TX_SERVICE_NAME_SIZE) || 100,
-        interval:{
-            denom:1000,
-            nft:1000,
-            validators: 1000,
-            faultTolerance:1000,
-            heartbeatRate:Number(HEARTBEAT_RATE || 10),
-        },
-        executeTime:{
-            denom:"1 * * * * *",
-            nft:"1 * * * * *",
-            txServiceName:'30 * * * * *',
-            validators:"1 * * * * *",
-            faultTolerance:"1 * * * * *",
-        }
     }
 };
