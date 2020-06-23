28e43d3bd00981794a7799b2cacdc9fcfa3a32c3
lsc949982212 <949982212@qq.com>
2020-06-23 19:06:41 +0800

    daemon

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { Logger } from '@nestjs/common';
-import cluster from 'cluster';
-import os from 'os';
 async function bootstrap(): Promise<void> {
-    if (cluster.isMaster) {
-        //the only thing to for master process is forking child process
-        console.log(`主进程 ${process.pid} 正在运行`);
-        for (let i = 0; i < os.cpus().length; i++) {
-            cluster.fork();
-        }
-        cluster.on('exit', (worker) => {
-            new Logger('Main.ts').error(`工作进程 ${worker.process.pid} 已退出`);
-        });
-    } else {
-        const app = await NestFactory.create(AppModule);
-        const options = new DocumentBuilder()
-            .setTitle('Tx example')
-            .setDescription('The tx API description')
-            .setVersion('1.0')
-            .addTag('tx')
-            .build();
-        const document = SwaggerModule.createDocument(app, options);
-        SwaggerModule.setup('api', app, document);
-        app.enableCors();
-        await app.listen(3000);
-        console.log(`工作进程 ${process.pid} 已启动`);
-    }
+    const app = await NestFactory.create(AppModule);
+    const options = new DocumentBuilder()
+        .setTitle('Tx example')
+        .setDescription('The tx API description')
+        .setVersion('1.0')
+        .addTag('tx')
+        .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api', app, document);
+    app.enableCors();
+    await app.listen(3000);
 }
 bootstrap();
