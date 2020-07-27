a4e82a43f5059591a7420d9f9b0533a6d8333147
hangTaishan <hangTaishan@bishuilantiandeMacBook-Pro.local>
2020-07-27 15:53:58 +0800

    #denom / nftName

--- src/dto/denom.dto.ts
+++ src/dto/denom.dto.ts
@@ -2,17 +2,18 @@ export class DenomResDto {
     name: string;
     schema: string;
     creator: string;
+    denom_name: string;
 
-    constructor(name: string, schema: string, creator: string){
+    constructor(name: string, schema: string, creator: string, denom_name:string){
         this.name = name;
+        this.denom_name = denom_name;
         this.schema = schema;
         this.creator = creator;
     }
 }
 
 export class DenomListResDto extends DenomResDto{
-    constructor(name: string, schema: string, creator: string){
-        super(name, schema,creator);
+    constructor(name: string, schema: string, creator: string, denom_name:string){
+        super(name, schema, creator, denom_name);
     }
 }
-
--- src/module/nft.module.ts
+++ src/module/nft.module.ts
@@ -4,6 +4,7 @@ import {NftService} from '../service/nft.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import {NftSchema} from '../schema/nft.schema';
 import { DenomSchema } from '../schema/denom.schema';
+import { NftMapSchema } from '../schema/nftMap.schema';
 
 @Module({
     imports:[
@@ -15,6 +16,11 @@ import { DenomSchema } from '../schema/denom.schema';
             name: 'Denom',
             schema: DenomSchema,
             collection: 'ex_sync_denom'
+        },
+        {
+            name: 'NftMap',
+            schema: NftMapSchema,
+            collection: 'ex_sync_nft_mapping'
         }])
     ],
     providers:[NftService],
