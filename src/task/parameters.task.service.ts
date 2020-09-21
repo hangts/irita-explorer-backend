614edffc4c88cd22f78315e9ccab4340dd37217f
zhjibi742626083 <jinbiao@bianjie.ai>
2020-09-21 10:32:03 +0800

    update parameters task

--- src/task/parameters.task.service.ts
+++ src/task/parameters.task.service.ts
@@ -1,45 +1,63 @@
-import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose'
+import {Injectable} from '@nestjs/common';
+import {InjectModel} from '@nestjs/mongoose';
+import {Model} from 'mongoose'
 import {StakingValidatorHttp} from "../http/lcd/staking.validator.http";
 import {getTimestamp} from "../util/util";
+import {moduleSlashing} from "../constant";
+
 @Injectable()
 export class ParametersTaskService {
     constructor(@InjectModel('ParametersTask') private parametersTaskModel: Model<any>
-                ,private readonly stakingValidatorHttp: StakingValidatorHttp) {
+        , private readonly stakingValidatorHttp: StakingValidatorHttp) {
         this.doTask = this.doTask.bind(this);
     }
-    async doTask(): Promise<void> {
+
+    async doTask(): Promise<any> {
         let parametersData = await this.stakingValidatorHttp.queryParametersFromSlashing(),
-            needInsertData = [];
+            needInsertData: any[] = [];
         let dbParametersData = await (this.parametersTaskModel as any).queryAllParameters()
         for (const parameterKey in parametersData) {
             const dbData = {
-                module:'slashing',
+                module: moduleSlashing,
                 key: parameterKey,
                 cur_value: parametersData[parameterKey],
-                create_time:getTimestamp(),
-                update_time:''
+                create_time: '',
+                update_time: ''
             }
-            needInsertData.push(dbData)
+            await needInsertData.push(dbData)
         }
-        if(dbParametersData.length === 0){
-            (this.parametersTaskModel as any).insertParameters(needInsertData)
-        }else {
-            this.handleData(dbParametersData,needInsertData)
+        if (dbParametersData.length === 0) {
+            needInsertData.forEach(item => {
+                item.create_time = getTimestamp()
+                item.update_time = getTimestamp()
+            })
+            await (this.parametersTaskModel as any).insertParameters(needInsertData)
+        } else {
+            await this.handleData(dbParametersData, needInsertData)
         }
     }
 
-    handleData(dbParameters,needInsertData){
-        if(needInsertData.keys().length > 0){
-            dbParameters.forEach( item => {
-                if(item.cur_value !== needInsertData[item.key]){
-                    needInsertData[item.key].update_time = getTimestamp()
+    handleData(dbParameters, needInsertData) {
+        let dbParametersMap = new Map()
+        let needInsertDataMap = new Map()
+        dbParameters.forEach(item => {
+            dbParametersMap.set(item.key, item)
+        })
+        needInsertData.forEach(item => {
+            needInsertDataMap.set(item.key, item)
+        })
+        if (needInsertDataMap.size > 0) {
+            dbParametersMap.forEach(item => {
+                if (needInsertDataMap.has(item.key)) {
+                    needInsertDataMap.get(item.key).update_time = getTimestamp()
+                } else {
+                    needInsertDataMap.get(item.key).create_time = getTimestamp()
                 }
             })
-            needInsertData.forEach( item => {
-                (this.parametersTaskModel as any).updateParameters(needInsertData)
-            })
         }
+        needInsertData.forEach(item => {
+            (this.parametersTaskModel as any).updateParameters(item)
+        })
+
     }
 }
