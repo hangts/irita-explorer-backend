c780b0f7492cec3a0c9f159ce1a3feeef2b97d9d
zhjibi742626083 <jinbiao@bianjie.ai>
2020-12-11 14:56:37 +0800

    error log is not displayed

--- src/schema/identity.schema.ts
+++ src/schema/identity.schema.ts
@@ -50,7 +50,9 @@ IdentitySchema.statics = {
   },
 
   async insertIdentityInfo(IdentityInfo) {
-      await  this.insertMany(IdentityInfo,{ ordered: false })
+      await  this.insertMany(IdentityInfo,{ ordered: false },(error) => {
+        //console.log(error)
+      })
   },
   // base information
   async updateIdentityInfo(updateIdentityData) {
