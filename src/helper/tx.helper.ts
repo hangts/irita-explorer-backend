9eb65e5e92e1a4d209acb383783cf59ec506428a
段劼 <65702143+duanjie-cmd@users.noreply.github.com>
2021-06-10 19:06:18 +0800

    update list msg

--- src/helper/tx.helper.ts
+++ src/helper/tx.helper.ts
@@ -125,7 +125,13 @@ export const dbRes = {
         events_new: 1,
         'msgs.msg.amount': 1,
         'msgs.msg.content.amount': 1,
-        'msgs.msg.initial_deposit':1
+        'msgs.msg.initial_deposit': 1,
+        'msgs.msg.sorce_port':1,
+        'msgs.msg.sorce_channel':1,//IBC
+        'msgs.msg.token':1,//IBC
+        'msgs.msg.timeout_height':1,//IBC
+        'msgs.msg.timeout_timestamp':1,//IBC
+        'msgs.msg.packet':1,//IBC
     },
     service:{
         ...common,
