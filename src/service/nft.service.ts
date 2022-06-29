0fdb2fcd3607b8253ccd445a49cd91b57bbf9003
junjie.chen <xiyueedu@gmail.com>
2022-06-29 18:46:50 +0800

    Fix query filed(denom_name) from nft about api nftList、nftDetail
    (cherry picked from commit 360f32eb10a3d8dd4900903541fd306a07ae871c)

--- src/service/nft.service.ts
+++ src/service/nft.service.ts
@@ -18,22 +18,31 @@ export class NftService {
         let nftData, count = null;
         if(pageNum && pageSize){
           nftData = await (this.nftModel as any).findList(pageNum, pageSize, denomId, nftId, owner, sort);
-          for (const nft of nftData?.data) {
+          if (nftData?.data.length === 0){
+                return new ListStruct(res, pageNum, pageSize, count);
+          }
+
+          const denomIds:string[] = Array.from(new Set(nftData.data.map(x => x.denom_id)));
+          const denoms:IDenomStruct[] = await (this.denomModel as any).findAllInDenomID(denomIds);
+          const denomIdNameMap = new Map<string, string>();
+          denoms.forEach(d => denomIdNameMap.set(d.denom_id,d.name))
+
+          for (const nft of nftData.data) {
               // let denomDetail = nft.denomDetail&&(nft.denomDetail as any).length > 0 ? nft.denomDetail[0] : null;
               const result = new NftListResDto(
                   nft.denom_id,
-                  nft.nft_id, 
-                  nft.owner, 
-                  nft.uri, 
-                  nft.data, 
+                  nft.nft_id,
+                  nft.owner,
+                  nft.uri,
+                  nft.data,
                   null,
-                  nft.denom_name,
+                  denomIdNameMap.get(nft.denom_id)||"",
                   nft.nft_name,
                   nft.last_block_time,
                   nft.create_time,
               );
               res.push(result);
-          }       
+          }
         }
         if(useCount){
             if (!denomId && !nftId && !owner) {
@@ -45,7 +54,7 @@ export class NftService {
                 count = await (this.nftModel as any).findListCount(denomId, nftId, owner);
             }
         }
-        
+
         return new ListStruct(res, pageNum, pageSize, count);
     }
 
@@ -57,12 +66,12 @@ export class NftService {
         if (nft) {
             return new NftDetailResDto(
                 nft.denom_id,
-                nft.nft_id, 
-                nft.owner, 
-                nft.uri, 
-                nft.data, 
+                nft.nft_id,
+                nft.owner,
+                nft.uri,
+                nft.data,
                 denomDetail,
-                nft.denom_name,
+                denomDetail.name,
                 nft.nft_name,
                 nft.last_block_time,
             );
